16bd9c14a8d7e4b8bd1b5781a629421f14a4fbb1
SudarshanAhire <sudarshanahire292@gmail.com>
2025-12-28 17:22:12 +0530

    commit1

---
+++ Assignment2/Assignment2_1.py
@@ -0,0 +1,13 @@
+""" 
+Write a program to display:
+ # Value
+ # Type
+ # Memory Address  
+for a variable using appropriate built-in functions. 
+""" 
+
+No = 11           # Variable
+
+print(No)         # 11
+print(type(No))   # <class 'int'
+print(id(No))     # 140718271235304
