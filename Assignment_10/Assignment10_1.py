a775dd81416ab5ffec4677ec7bd18926a662de40
SudarshanAhire <sudarshanahire292@gmail.com>
2026-01-16 19:36:33 +0530

    commit1

--- Assignment_10/Assignment10_1.py
+++ Assignment_10/Assignment10_1.py
@@ -1,13 +1,10 @@
-def PrintTable(No):
+def main():
+    No = 0
+    No = int(input("Enter number : ")) 
 
     for i in range(1, 11):
         print(No * i, end=" ")
 
-def main():
-    No = 0
-    No = int(input("Enter number : ")) 
     
-    PrintTable(No)
-
 if __name__ == "__main__":
     main()
