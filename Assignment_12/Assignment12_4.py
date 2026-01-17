7addd8e608c72f813fac01869327be3cc65805c0
SudarshanAhire <sudarshanahire292@gmail.com>
2026-01-17 10:20:43 +0530

    commit1

---
+++ Assignment_12/Assignment12_4.py
@@ -0,0 +1,9 @@
+def main():
+    No = 0
+    No = int(input("Enter Number : "))
+
+    for i in range(1, No+1):
+        print(i, end=" ")
+
+if __name__ == "__main__":
+    main()
---
+++ Assignment_12/Assignment12_5.py
@@ -0,0 +1,9 @@
+def main():
+    No = 0
+    No = int(input("Enter Number : "))
+
+    for i in range(No, 0, -1):
+        print(i, end=" ") 
+
+if __name__ == "__main__":
+    main()
