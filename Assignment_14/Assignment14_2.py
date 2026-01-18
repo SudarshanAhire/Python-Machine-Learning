7e03715fec161842c411414045d82ea81a24fbc1
SudarshanAhire <sudarshanahire292@gmail.com>
2026-01-18 11:16:25 +0530

    commit1

---
+++ Assignment_14/Assignment14_2.py
@@ -0,0 +1,13 @@
+
+Cube = lambda No : No * No * No
+
+def main():
+    No = 0
+    No = int(input("Enter number : "))
+
+    Ret = Cube(No)
+
+    print("Cube is : ", Ret)
+
+if __name__ == "__main__":
+    main()
