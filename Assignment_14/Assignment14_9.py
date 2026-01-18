045ffe958ae2cd63647104f4974a63589896432f
SudarshanAhire <sudarshanahire292@gmail.com>
2026-01-18 11:54:29 +0530

    commit1

---
+++ Assignment_14/Assignment14_9.py
@@ -0,0 +1,17 @@
+
+Multiplication = lambda A, B : A * B
+
+def main():
+    No1 = 0
+    No2 = 0
+
+    No1 = int(input("Enter first number : "))
+    No2 = int(input("Enter second number : "))
+
+    Ret = Multiplication(No1, No2)
+
+    print("Multiplication is : ", Ret)
+
+
+if __name__ == "__main__":
+    main()
