ed2cbdd5ab2e5488a8f6a1fdd1b0c1c42fe33b0c
SudarshanAhire <sudarshanahire292@gmail.com>
2026-01-27 09:53:50 +0530

    commit1

--- Assignment_23/Assignment23_1.py
+++ Assignment_23/Assignment23_1.py
@@ -1,1 +1,21 @@
-print("Jay Ganesh...")
+class BookStore:
+
+    NoOfBooks = 0
+
+    def __init__(self, Book_Name, Book_Author):
+        self.Name = Book_Name 
+        self.Author = Book_Author
+        BookStore.NoOfBooks = BookStore.NoOfBooks + 1
+
+    def Display(self):
+        print(f"{self.Name} by {self.Author}. No of books: {BookStore.NoOfBooks}")
+
+
+obj1 = BookStore("Linux System Programming", "Robert Love")
+obj1.Display()
+
+obj2 = BookStore("C Programming", "Dennis Ritchie")
+obj2.Display()
+
+
+
