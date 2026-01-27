f8641e096653d6b5c3f199f42228b97583a00ac0
SudarshanAhire <sudarshanahire292@gmail.com>
2026-01-27 09:41:40 +0530

    commit1

--- Assignment_22/Assignment22_1.py
+++ Assignment_22/Assignment22_1.py
@@ -1,1 +1,27 @@
-print("Jay Ganesh...")
+class Demo:
+    Value = 0  # class veriable
+
+    def __init__(self, A, B):
+        self.No1 = A 
+        self.No2 = B
+
+    def Fun(self):
+        print("Inside fun")
+        print("Value of No1 :", self.No1)
+        print("Value of No2 :", self.No2)
+
+    def Gun(self):
+        print("Inside gun")
+        print("Value of No1 :", self.No1)
+        print("Value of No2 :", self.No2)
+
+obj1 = Demo(10, 11)
+obj2 = Demo(51, 101)
+
+obj1.Fun()
+obj2.Fun()
+
+obj1.Gun()
+obj2.Gun()
+    
+    
---
+++ Assignment_22/Assignment22_2.py
@@ -0,0 +1,38 @@
+class Circle:
+    PI = 3.14
+
+    def __init__(self):
+        self.Radius = 0.0        
+        self.Area = 0.0
+        self.Circumference = 0.0 
+
+    def Accept(self):
+        self.Radius = float(input("Enter radius : "))
+
+    def CalculateArea(self):
+        self.Area = Circle.PI * self.Radius * self.Radius
+                                        
+    def CalculateCircumference(self):
+        self.Circumference = 2 * Circle.PI * self.Radius
+
+    def Display(self):
+        print("Radius of Circle :", self.Radius)
+        print("Area of circle :", self.Area)
+        print("Circumference of circle :", self.Circumference)
+
+obj1 = Circle()
+
+obj1.Accept()
+obj1.CalculateArea()
+obj1.CalculateCircumference()
+obj1.Display()
+
+obj2 = Circle()
+obj2.Accept()
+obj2.CalculateArea()
+obj2.CalculateCircumference()
+obj2.Display()
+        
+
+
+
