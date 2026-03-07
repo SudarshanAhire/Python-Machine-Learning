399179513f1e488323bcf5f47d64963d1c5ccc50
SudarshanAhire <sudarshanahire292@gmail.com>
2026-03-08 02:05:12 +0530

    commit1

--- Assignment_45/Assignment45_1.py
+++ Assignment_45/Assignment45_1.py
@@ -1,54 +1,116 @@
-import pandas as pd
+import pandas as pd 
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.metrics import accuracy_score
 
-def main():
-    Border = "-"*50
+def WinePredictor(FilePath):
+    Border = "-"*40
+
+    #--------------------------------------------------------------
+    # Step 1 - Laod dataset
+    #--------------------------------------------------------------
+    print(Border)
+    print("Step 1 - Load Dataset")
     print(Border)
 
-    df = pd.read_csv("WinePredictor.csv")
+    df = pd.read_csv(FilePath)
 
-    print("Shape of dataset :", df.shape)
-
-    print("First five records in Dataset :")
+    print("Few records from file :")
     print(df.head())
+
+    #--------------------------------------------------------------
+    # Step 2 - Check missing values
+    #--------------------------------------------------------------
+    print(Border)
+    print("Step 2 - Check missing values")
     print(Border)
 
-    print("Analysis of dataset :", df.describe())
-    print(Border)
-
-    print("Null value count (per column)")
+    print("Missing count :")
     print(df.isnull().sum())
+
+    #--------------------------------------------------------------
+    # Step 3 - Display Statistical summury
+    #--------------------------------------------------------------
+    print(Border)
+    print("Step 3 - Display Statistical summury")
     print(Border)
 
-    # Alcohol,Malic acid,Ash,Alcalinity of ash,Magnesium,Total phenols,Flavanoids,Nonflavanoid phenols,Proanthocyanins,Color intensity,Hue,OD280/OD315 of diluted wines,Proline
+    print(df.describe())
+
+    #-----------------------------------------------------------------
+    # Step 4 - Correlation between columns
+    #-----------------------------------------------------------------
+    print(Border)
+    print("Step 4 - correlation between columns")
+    print(Border)
+
+    print("Correlation matrix")
+    print(df.corr())
+
+    #-----------------------------------------------------------------
+    # Step 5 - Spliting dataset into depedndent and idependent variables
+    #-----------------------------------------------------------------
+    print(Border)
+    print("Step 5 - Spliting dataset into depedndent and idependent variables")
+    print(Border)
 
     X = df[['Alcohol', 'Malic acid', 'Ash', 'Alcalinity of ash', 'Magnesium', 'Total phenols', 'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity', 'Hue', 'OD280/OD315 of diluted wines', 'Proline']]
+    Y = df['Class']
 
-    Y = df[['Class']]
+    print("Shape of independent variables :", X.shape)
+    print("Shape of dependent variables :", Y.shape)
+
+    #-----------------------------------------------------------------
+    # Step 6 - Split the dataset into traing and testing
+    #-----------------------------------------------------------------
+    print(Border)
+    print("Step 6 - Split the dataset into traing and testing")
+    print(Border)   
 
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+    print("X_train shape :", X_train.shape)
+    print("X_test shape :", X_test.shape)
+    print("Y_train shape :", Y_train.shape)
+    print("Y_test shape :", Y_test.shape)
+
+
+    #-----------------------------------------------------------------
+    # Step 7 - Create and train the model
+    #-----------------------------------------------------------------
+    print(Border)
+    print("Step 7 - Create and train the model")
+    print(Border)   
 
     model = KNeighborsClassifier(n_neighbors=3)
 
     model.fit(X_train, Y_train)
 
-    Y_pred = model.predict(X_test)
-
-    print("Predicted Class :")
-    print(Y_pred)
+    #-----------------------------------------------------------------
+    # Step 8 - Test the model
+    #-----------------------------------------------------------------
+    print(Border)
+    print("Step 8 - Test the model")
     print(Border)
 
-    print("Actual Class are :")
-    print(Y_test)
+    Y_pred = model.predict(X_test)
+
+
+    #-----------------------------------------------------------------
+    # Step 8 - Evaluate the model
+    #-----------------------------------------------------------------
+    print(Border)
+    print("Step 8 - Evaluate the model")
+    print(Border)
 
     Accuracy = accuracy_score(Y_test, Y_pred)
 
     print("Accuracy of the model :", Accuracy*100)
 
+
+def main():
+    WinePredictor("WinePredictor.csv")
 
 if __name__ == "__main__":
     main()
