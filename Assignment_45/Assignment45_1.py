2a30ae818819f324997636b8a9eee0c47d6301bf
SudarshanAhire <sudarshanahire292@gmail.com>
2026-03-09 16:43:44 +0530

    commit1

--- Assignment_45/Assignment45_1.py
+++ Assignment_45/Assignment45_1.py
@@ -1,116 +1,199 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.preprocessing import StandardScaler
 
-def WinePredictor(FilePath):
+def MarvellousClassifier(DataPath):
     Border = "-"*40
 
-    #--------------------------------------------------------------
-    # Step 1 - Laod dataset
-    #--------------------------------------------------------------
+    #------------------------------------------------------------------------------------
+    # Strp 1 - Load the dataset from csv file
+    #------------------------------------------------------------------------------------
+
     print(Border)
-    print("Step 1 - Load Dataset")
+    print("Strp 1 - Load the dataset from csv file")
     print(Border)
 
-    df = pd.read_csv(FilePath)
-
-    print("Few records from file :")
+    df = pd.read_csv(DataPath)
+    print(Border)
+    print("Some Entries from dataset")
     print(df.head())
-
-    #--------------------------------------------------------------
-    # Step 2 - Check missing values
-    #--------------------------------------------------------------
-    print(Border)
-    print("Step 2 - Check missing values")
     print(Border)
 
-    print("Missing count :")
-    print(df.isnull().sum())
+    #------------------------------------------------------------------------------------
+    # Step 2 - Clean the dataset from removing empty rows
+    #------------------------------------------------------------------------------------
 
-    #--------------------------------------------------------------
-    # Step 3 - Display Statistical summury
-    #--------------------------------------------------------------
     print(Border)
-    print("Step 3 - Display Statistical summury")
+    print("Step 2 - Clean the Dataset from removing empty rows")
     print(Border)
 
-    print(df.describe())
-
-    #-----------------------------------------------------------------
-    # Step 4 - Correlation between columns
-    #-----------------------------------------------------------------
-    print(Border)
-    print("Step 4 - correlation between columns")
+    df.dropna(inplace=True)
+    print("Total records :", df.shape[0])
+    print("Total clumns :", df.shape[1])
     print(Border)
 
-    print("Correlation matrix")
-    print(df.corr())
+    #------------------------------------------------------------------------------------
+    # Step 3 - Separeate independeant and dependant variables
+    #------------------------------------------------------------------------------------
 
-    #-----------------------------------------------------------------
-    # Step 5 - Spliting dataset into depedndent and idependent variables
-    #-----------------------------------------------------------------
     print(Border)
-    print("Step 5 - Spliting dataset into depedndent and idependent variables")
+    print("Step 3 - Separeate independeant and dependant variables")
     print(Border)
 
-    X = df[['Alcohol', 'Malic acid', 'Ash', 'Alcalinity of ash', 'Magnesium', 'Total phenols', 'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity', 'Hue', 'OD280/OD315 of diluted wines', 'Proline']]
+    X = df.drop(columns=['Class'])
     Y = df['Class']
 
-    print("Shape of independent variables :", X.shape)
-    print("Shape of dependent variables :", Y.shape)
+    print("Shape of X :", X.shape)
+    print("Shape of Y :", Y.shape)
 
-    #-----------------------------------------------------------------
-    # Step 6 - Split the dataset into traing and testing
-    #-----------------------------------------------------------------
     print(Border)
-    print("Step 6 - Split the dataset into traing and testing")
-    print(Border)   
+    print("Input columns :", X.columns.tolist())
+    print("Output column : Class")
 
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    #------------------------------------------------------------------------------------
+    # Step 4 - Split the dataset for training and testing
+    #------------------------------------------------------------------------------------
 
+    print(Border)
+    print("Step 4 - Split the dataset for training and testing")
+    print(Border)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42, stratify=Y)
+
+    print(Border)
+    print("Information of training and testind data")
     print("X_train shape :", X_train.shape)
     print("X_test shape :", X_test.shape)
     print("Y_train shape :", Y_train.shape)
     print("Y_test shape :", Y_test.shape)
 
+    #------------------------------------------------------------------------------------
+    # Step 5 - Feature Scalling
+    #------------------------------------------------------------------------------------
 
-    #-----------------------------------------------------------------
-    # Step 7 - Create and train the model
-    #-----------------------------------------------------------------
     print(Border)
-    print("Step 7 - Create and train the model")
-    print(Border)   
-
-    model = KNeighborsClassifier(n_neighbors=3)
-
-    model.fit(X_train, Y_train)
-
-    #-----------------------------------------------------------------
-    # Step 8 - Test the model
-    #-----------------------------------------------------------------
-    print(Border)
-    print("Step 8 - Test the model")
+    print("Step 5 - Feature Scalling")
     print(Border)
 
-    Y_pred = model.predict(X_test)
+    scaler = StandardScaler()
+    # Independent variables scalling
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.fit_transform(X_test)
 
+    print("Feature Scalling is done")
 
-    #-----------------------------------------------------------------
-    # Step 8 - Evaluate the model
-    #-----------------------------------------------------------------
+    #------------------------------------------------------------------------------------
+    # Step 6 - Explore the multiple values of K
+    # HyperParameter tunning (K)
+    #------------------------------------------------------------------------------------
+
+    accuracy_scores = []
+    K_values = range(1, 21)
+
+    for k in K_values:
+        model = KNeighborsClassifier(n_neighbors=k)
+
+        model.fit(X_train_scaled, Y_train)
+
+        Y_pred = model.predict(X_test_scaled)
+
+        accuracy = accuracy_score(Y_test, Y_pred)
+
+        accuracy_scores.append(accuracy*100)
+
     print(Border)
-    print("Step 8 - Evaluate the model")
+    print("Accuracy report of all k values from 1 to 20")
+    for value in accuracy_scores:
+        print(value)
+
     print(Border)
 
-    Accuracy = accuracy_score(Y_test, Y_pred)
+    #------------------------------------------------------------------------------------
+    # Step 7 - Plot graph of k vs accuarcy
+    #------------------------------------------------------------------------------------
+    print(Border)
+    print("Step 7 - Plot graph of k vs accuarcy")
+    print(Border)
 
-    print("Accuracy of the model :", Accuracy*100)
+    plt.figure(figsize=(8, 5))
+    plt.plot(K_values, accuracy_scores, marker='o')
+    plt.xlabel("K Values")
+    plt.ylabel("Accuracy Scores")
+    plt.title("K Values vs Accuracy Scores")
+    plt.grid(True)
+    plt.xticks(list(K_values))
+    plt.show()
+
+    #------------------------------------------------------------------------------------
+    # Step 8 - find best values of k
+    #------------------------------------------------------------------------------------
+    print(Border)
+    print("Step 8 - find best values of k")
+    print(Border)
+
+    best_k = list(K_values)[accuracy_scores.index(max(accuracy_scores))]
+
+    print("Best value of k is :", best_k)
+
+    #------------------------------------------------------------------------------------
+    # Step 9 - Build Final model using best value of k 
+    #------------------------------------------------------------------------------------
+    print(Border)
+    print("Step 9 - Build Final model using best value of k") 
+    print(Border)
+
+    final_model = KNeighborsClassifier(n_neighbors=best_k)
+
+    final_model.fit(X_train_scaled, Y_train)
+
+    Y_pred = final_model.predict(X_test_scaled)
+
+    #------------------------------------------------------------------------------------
+    # Step 10 - Calculate final accuracy 
+    #------------------------------------------------------------------------------------
+    print(Border)
+    print("Step 10 - Calculate final accuracy") 
+    print(Border)
+
+    accuracy = accuracy_score(Y_test, Y_pred)
+
+    print("Accuracy of the model is :", accuracy*100)
+
+    #------------------------------------------------------------------------------------
+    # Step 11 - Display confusion matrics 
+    #------------------------------------------------------------------------------------
+    print(Border)
+    print("Step 11 - Display confusion matrics") 
+    print(Border)
+
+    cm = confusion_matrix(Y_test, Y_pred)
+
+    print(cm)
+
+    #------------------------------------------------------------------------------------
+    # Step 12 - Display classification report 
+    
+    print(Border)
+    print("Step 12 - Display classification report") 
+    print(Border)
+
+    print(classification_report(Y_test, Y_pred))
+
+    print(Border)
 
 
 def main():
-    WinePredictor("WinePredictor.csv")
+    Border = "-"*40
+    print(Border)
+    print("Wine Classifier using KNN")
+    print(Border)
+
+    MarvellousClassifier("WinePredictor.csv")
 
 if __name__ == "__main__":
     main()
