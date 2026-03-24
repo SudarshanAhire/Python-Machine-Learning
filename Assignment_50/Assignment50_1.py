e24c2fc6974c5939636e6650fbe8d26ee6d97197
SudarshanAhire <sudarshanahire292@gmail.com>
2026-03-25 00:49:09 +0530

    commit1

--- Assignment_50/Assignment50_1.py
+++ Assignment_50/Assignment50_1.py
@@ -2,7 +2,16 @@ import pandas as pd
 import numpy as np
 
 def main():
-    pass 
+    data = pd.read_csv("bank-full.csv", sep=";")
+    data.to_csv("converted.csv", index=False)
+
+    df = pd.read_csv("converted.csv")
+
+    print(df.head())
+    print(df.shape)
+
+    print("MIssing values count :", df.isnull().sum())
+    
 
 if __name__ == "__main__":
     main()
