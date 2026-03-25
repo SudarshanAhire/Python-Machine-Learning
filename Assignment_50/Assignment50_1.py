abedb1331e6cde480b2a3f792b8596b495fca45b
SudarshanAhire <sudarshanahire292@gmail.com>
2026-03-26 00:56:16 +0530

    commit1

--- Assignment_50/Assignment50_1.py
+++ Assignment_50/Assignment50_1.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 def main():
 
@@ -146,8 +150,20 @@ def main():
     #------------------------------------------------------------
 
     X_train, X_test, Y_train, Y_test = train_test_split(X_scaled, Y, test_size=0.2, random_state=42)
-    
-            
+
+    #------------------------------------------------------------
+    # Step 7 - Training classification models
+    #------------------------------------------------------------
+
+    model = KNeighborsClassifier(n_neighbors=5)
+
+    model.fit(X_train, Y_train)
+
+    Y_pred = model.predict(X_test)
+
+    acc = accuracy_score(Y_test, Y_pred)
+
+    print("Accuracy :", acc)
     
 
 if __name__ == "__main__":
