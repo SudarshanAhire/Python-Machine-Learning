99df0787ae6c5c1910be3d2780823e7271d7f19a
SudarshanAhire <sudarshanahire292@gmail.com>
2026-03-26 22:45:57 +0530

    commit1

--- Assignment_51/Assignment51_1.py
+++ Assignment_51/Assignment51_1.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import BaggingClassifier
 
 def main():
     #------------------------------------------------------
@@ -48,20 +49,43 @@ def main():
     print(df.isnull().sum())
 
     #------------------------------------------------------
-    # Step 3 - Feature Extraction 
+    # Step 3 - Feature Extraction And TF-IDF Vectorization
     #------------------------------------------------------
 
-    X = df[['title', 'text']]
+    X = df['title'] + " " + df['text']
     Y = df['label']
 
     vectorizer = TfidfVectorizer()
 
     X = vectorizer.fit_transform(X)
 
+    #------------------------------------------------------
+    # Step 4 - Model Building
+    #------------------------------------------------------
 
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
+    lr_model = LogisticRegression(max_iter=1000)
+    dt_model = DecisionTreeClassifier(random_state=42)
 
+    lr_model.fit(X_train, Y_train)
+    dt_model.fit(X_train, Y_train)
 
+    hard_model = BaggingClassifier(
+        estimator = [
+        ('lr', lr_model),
+        ('dt', dt_model)
+    ],
+    voting = 'hard'
+    )
+
+    hard_model.fit(X_train, Y_train)
+
+    Y_pred = hard_model.predict(X_test)
+
+    acc = accuracy_score(Y_test, Y_pred)
+
+    print(acc)
 
 
 if __name__ == "__main__":
