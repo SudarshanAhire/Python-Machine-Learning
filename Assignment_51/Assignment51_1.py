98076d94556df80119f55fde2c40f529ce850c5f
SudarshanAhire <sudarshanahire292@gmail.com>
2026-03-26 19:55:12 +0530

    commit1

--- Assignment_51/Assignment51_1.py
+++ Assignment_51/Assignment51_1.py
@@ -10,14 +10,37 @@ def main():
     #------------------------------------------------------
     # Step 1 - Load Dataset and Preprocessing
     #------------------------------------------------------
-    
+
     true_data = pd.read_csv("True.csv")
 
     fake_data = pd.read_csv("Fake.csv")
 
+    print("Dataset entris before adding lables :")
+    print(true_data.head())
+
     true_data['label'] = 1
+    print("Dataset entris after adding lables :")
+    print(true_data.head())
+
+    print("Dataset entris before adding lables :")
+    print(fake_data.head())
 
     fake_data['label'] = 0
+    print("Dataset entris after adding lables :")
+    print(fake_data.head())
+
+    data = [true_data, fake_data]
+
+    df = pd.concat(data)
+
+    print("Dataset after concatinating :")
+    print(df.head(10))
+
+    #------------------------------------------------------
+    # Step 2 - Analysis and Preprocessing Of Dataset
+    #------------------------------------------------------
+
+    
 
 
 if __name__ == "__main__":
