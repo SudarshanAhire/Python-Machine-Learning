9775b8c5e57b291b6271f5a7dda065f362562424
SudarshanAhire <sudarshanahire292@gmail.com>
2026-04-02 19:55:19 +0530

    commit1

--- Assignment_52/Assignment52_1.py
+++ Assignment_52/Assignment52_1.py
@@ -11,49 +11,62 @@ def main():
     #----------------------------------------------------------
     # Step 1 - Load the dataset
     #----------------------------------------------------------
-
+    Border = "-"*50
+    print(Border)
     print("Step 1 - Load the dataset")
+    print(Border)
+
     df = pd.read_csv("student-mat.csv", sep=";")
 
     print("First few records of dataset :")
     print(df.head())
+    print(Border)
 
     print("Shape of the dataset :")
     print(df.shape)
+    print(Border)
 
     print("Missing values of dataset :")
     print(df.isnull().sum())
+    print(Border)
 
     #----------------------------------------------------------
     # Step 2 - Select the features (independent variables)
     #----------------------------------------------------------
+    print("Step 2 - Select the features ")
+    print(Border)
 
-    print("Step 2 - Select features")
-
+    print("Data after feature selection :")
     X = df[['G1', 'G2', "G3",'studytime', 'absences', 'failures']]
     print(X.head())
+    print(Border)
 
     print("Shape of the features :")
     print(X.shape)
+    print(Border)
 
     print("Statistical information of the dataset :")
     print(X.describe())
+    print(Border)
 
     #------------------------------------------
     # Step 3 - Scaled the data
     #------------------------------------------
     print("Step 3 - Scaled the data")
+    print(Border)
 
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
 
     print("Data after scalling :")
     print(X_scaled[:5])
+    print(Border)
 
    #------------------------------------------
     # Step 4 - train the model
     #------------------------------------------
     print("Step 4 - Train the model")
+    print(Border)
 
     model = KMeans(n_clusters=3, random_state=42, n_init=10)
     clusters = model.fit_predict(X_scaled)
@@ -62,8 +75,10 @@ def main():
 
     print("Dataset with clusters")
     print(df.head(30))
+    print(Border)
 
     print(df[['G1', 'G2', 'G3', 'studytime', 'absences', 'failures', 'clusters']])
+    print(Border)
    
     
 if __name__ == "__main__":
